77c24c429cc59634689f9cccbb51fd94487e6366
zhangshaoxuan <web_zsx@163.com>
2018-02-09 10:04:12 +0800

    my single app test

---
+++ singleapp/js/source/components/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// var Logo = React.createClass({
+// 	render: function(){
+// 		return <div className="Logo" />;
+// 	}
+// })
+	class Logo extends React.Component {
+		render() {
+			return <div className="Logo" />;
+		}
+	}
+
+export default Logo
